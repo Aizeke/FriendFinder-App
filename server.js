e4eba5f7baebe08827b12b35b251a36e2103c914
Aizeke <iguido97@icloud.com>
2019-08-17 18:24:14 -0700

    Fixed file issues, correct directories work now.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;;
@@ -6,70 +7,18 @@ var PORT = process.env.PORT || 3000;;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var personList = [{
-    name: "Ahmed",
-    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-    scores: [1, 5, 4, 4, 2, 4, 5, 2, 3, 4]
-}];
-
-var displayBestMatch = function (person) {
-
-}
-
-$(".submission").on("click", function (event) {
-    event.preventDefault();
-    var bestMatch;
-    var totalDifference = 0;
-
-    var newPerson = {
-        name: $('#inputName').val().trim(),
-        photo: $('#inputImage').val().trim(),
-        scores: [$('#q-1').val(),
-        $('#q-2').val(),
-        $('#q-3').val(),
-        $('#q-4').val(),
-        $('#q-5').val(),
-        $('#q-6').val(),
-        $('#q-7').val(),
-        $('#q-8').val(),
-        $('#q-9').val(),
-        $('#q-10').val()]
-    }
-     console.log(newPerson);
-    // needs work on
-
-    for (var i = 0; i < personList.length; i++) {
-        totalDifference = 0;
-        for (var j = 0; j < personList.length; j++) {
-            var difference;
-            difference += Math.abs(newPerson.scores[j] - personList[i].scores[j]);
-            if (totalDifference < difference) {
-                bestMatch = personList[i].name;
-            }
-        }
-    }
-
-    displayBestMatch(bestMatch);
-
-    personList.push(newPerson);
-});
-
+// Get the Home Page HTML.
 app.get('/', function (req, res) {
-
-    res.sendFile("/home.html", { root: __dirname });
+    res.sendFile(path.join(__dirname, "./app/public", "home.html"));
+});
+// Get the Survey HTML page. 
+app.get("/survey",function(req,res){
+    res.sendFile(path.join(__dirname, "./app/public", "survey.html"))
 });
 
-$(".view-survey").on("click", function (event) {
-    event.preventDefault();
-    app.get('/survey', function (req, res) {
-
-        res.sendFile("/survey.html", { root: __dirname });
-    });
-})
-
-
+// Get Friends Object.
 app.get("/api/friends", function (req, res) {
-    return res.json(personList);
+    return res.send("./friends.js");
 });
 
 app.listen(PORT, function () {
